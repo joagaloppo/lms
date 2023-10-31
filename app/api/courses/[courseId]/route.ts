49db43a296f908797926d1b090fb4781dee73e38
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-10-31 09:29:55 -0300

    Add course edit title and description

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(req: Request, { params }: { params: { courseId: string } }) {
+    try {
+        const { userId } = auth();
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+        const values = await req.json();
+
+        const course = await db.course.update({
+            where: {
+                id: params.courseId,
+                userId,
+            },
+            data: {
+                ...values,
+            },
+        });
+
+        return NextResponse.json(course);
+    } catch (error) {
+        console.log("[COURSE_ID]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
