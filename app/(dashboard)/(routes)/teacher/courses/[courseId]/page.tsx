f6af579ee00112e3d3c07594842cf8ac25e628b2
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-10-31 17:27:12 -0300

    Add price form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,12 +1,13 @@
 import IconBadge from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
+import PriceForm from "./_components/price-form";
 
 interface PageProps {
     params: {
@@ -53,7 +54,7 @@ const Page: React.FC<PageProps> = async ({ params }) => {
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                 <div>
-                    <div className="flex items-center gap-x-2">
+                    <div className="flex items-center gap-x-4">
                         <IconBadge icon={LayoutDashboard} />
                         <h2 className="text-xl font-medium">Customize your course</h2>
                     </div>
@@ -65,6 +66,22 @@ const Page: React.FC<PageProps> = async ({ params }) => {
                         courseId={course.id}
                         options={categories.map((category) => ({ label: category.name, value: category.id }))}
                     />
+                </div>
+                <div className="space-y-6">
+                    <div>
+                        <div className="flex items-center gap-x-4">
+                            <IconBadge icon={ListChecks} />
+                            <h2 className="text-xl font-medium">Course chapters</h2>
+                        </div>
+                        <PriceForm initialData={course} courseId={course.id} />
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-x-4">
+                            <IconBadge icon={CircleDollarSign} />
+                            <h2 className="text-xl font-medium">Sell your course</h2>
+                        </div>
+                        <PriceForm initialData={course} courseId={course.id} />
+                    </div>
                 </div>
             </div>
         </div>
--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -2,7 +2,7 @@ import { LucideIcon } from "lucide-react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const backgroundVariants = cva("rounded-full flex items-center justify-center", {
+const backgroundVariants = cva("rounded-lg flex items-center justify-center", {
     variants: {
         variant: {
             default: "bg-sky-100",
